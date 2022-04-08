9c3f406f4bea2ce42fbb352c90133758249cea39
Junior Barros <juniorbaarros@gmail.com>
2022-04-08 03:04:28 -0300

    fix: bug in login and session

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -2,13 +2,23 @@ class SessionController < ApplicationController
   before_action :set_user, only: %i[ auth ]
 
   def auth
-    return if session[:user_auth]
-    users = session[:users_list]
-    user = user_exists?(users)
-    return if !user || user.password != @user.password
-    session[:user_auth] = SecureRandom.base64(32)
-    session[:user] = user
-    redirect_to home_path
+    if user_params["email"].empty? || user_params["password"].empty?
+      @errors = "Password or Email are empty"
+      render template: "users/index", status: :unprocessable_entity
+    else
+      return if session[:user_auth]
+
+      users = session[:users_list]
+      user = user_exists?(users)
+      if user && @user.password.casecmp?(user[:password])
+        session[:user_auth] = SecureRandom.base64(32)
+        session[:user] = user
+        redirect_to home_path
+      else
+      @errors = "Email or Password are invalid!!"
+      render template: "users/index", status: :unprocessable_entity
+      end
+    end
   end
 
   def logout
@@ -22,9 +32,9 @@ class SessionController < ApplicationController
   private
     def user_exists? users
       users = transform_to_object(users)
-      return if users.empty?
+      return false if users.empty?
       users.each do |user|
-        if user.email == @user.email
+        if user["email"].casecmp?(@user.email)
           return user
         end
       end
